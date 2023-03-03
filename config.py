370d86767c47ced0338bc7984bdeecf417d13430
Alexandr <119510050+akhalturin@users.noreply.github.com>
2023-03-03 20:41:00 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = "6125024227:AAGf2LBVs6P5cJgy2Wb31wnGZRlrJCo0f-I"
+CURRENCY_API = 'c699c1bde1dd304c11cd646a54220fb3'
+#AVALIABLE CURRENCY
+val = {
+    'евро' : 'EUR',
+    'рубль' : 'RUB',
+    'доллар' : 'USD'
+}
+symbols = {
+    'EUR' : '€',
+    'RUB' : '₽',
+    'USD' : '$'
+}
